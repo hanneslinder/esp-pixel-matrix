ba59121ae481f10e43f05f6263ec5302359abc7b
= <=>
2022-05-10 21:13:22 +0200

    Add support for http basic auth when checking for new version

--- browser/config.example.ts
+++ browser/config.example.ts
@@ -2,6 +2,10 @@ export const config = {
   unsplashApiKey: "xxx-xxx-xxx-xxx", // Add unsplash api key if you want to be able to query random images https://unsplash.com/developers
   updates: { 
     versionUrl: "", // check for new version updates from this url
-    downloadUrl: "", // download pager for new versions
+    downloadUrl: "", // download pager for new versions,
+    credentials: { // (optional) http basic auth credentials
+      username: "litgrid",
+      password: "TheGridIsLit"
+    }
   }
 }
--- browser/src/components/utils/VersionChecker.tsx
+++ browser/src/components/utils/VersionChecker.tsx
@@ -7,16 +7,34 @@ import "./VersionChecker.less";
 const localVersion = (window as any).version;
 interface Props {}
 
+const hasAuthCredentials = () => config.updates.credentials && config.updates.credentials.username && config.updates.credentials.password;
+
+const getDownloadUrl = () => {
+	if (hasAuthCredentials()) {
+		const [protocol, domain] = config.updates.downloadUrl.split("://");
+		return `${protocol}://${config.updates.credentials.username}:${config.updates.credentials.password}@${domain}`;
+	} else {
+		return config.updates.downloadUrl;
+	}
+}
+
 export const VersionChecker: React.FC<Props> = () => {
 	const [didUpdateCheck, setUpdateCheck] = useState(false);
 	const [versionNumber, setNewVersionNumber] = useState(localVersion);
 	const [newVersionAvailable, setNewVersionAvailable] = useState(false);
 
 	useEffect(() => {
-		fetch(config.updates.versionUrl)
+		const fetchConfig: any = { method: "GET" };
+
+		if (hasAuthCredentials()) {
+			let headers = new Headers();
+			headers.set("Authorization", "Basic " + window.btoa(config.updates.credentials.username + ":" + config.updates.credentials.password));
+			fetchConfig.headers = headers;
+		}
+
+		fetch(config.updates.versionUrl, fetchConfig)
 			.then((response) => response.json())
 			.then((json) => {
-				console.log(json);
 				setUpdateCheck(true);
 
 				if (json[0].version !== localVersion) {
@@ -24,11 +42,11 @@ export const VersionChecker: React.FC<Props> = () => {
 					setNewVersionAvailable(true);
 				}
 			});
-	}, [didUpdateCheck]);
+	}, []);
 
 	return (
 		<div className="version-checker">
-			{newVersionAvailable && <a className="new-version" href={config.updates.downloadUrl} target="_blank">{`New Version ${versionNumber} available!`}</a>}
+			{newVersionAvailable && <a className="new-version" href={getDownloadUrl()} target="_blank">{`New Version ${versionNumber} available!`}</a>}
 			{!newVersionAvailable && <div>{`version ${versionNumber}`}</div>}
 			{!didUpdateCheck && <Loader />}
 		</div>
