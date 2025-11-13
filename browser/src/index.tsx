0e77273328a228f524cca2b3b93229fef9c390f1
= <=>
2025-11-13 15:43:33 +0100

    add tailwind css

--- browser/src/index.tsx
+++ browser/src/index.tsx
@@ -2,7 +2,10 @@ import React, { useRef } from "react";
 import ReactDOM from "react-dom";
 import { view } from "@risingstack/react-easy-state";
 
-import { CanvasWrapper, CanvasWrapperElement } from "./components/canvas/CanvasWrapper";
+import {
+  CanvasWrapper,
+  CanvasWrapperElement,
+} from "./components/canvas/CanvasWrapper";
 import { Header } from "./components/Header";
 import { SidebarRight } from "./components/sidebar-right/SidebarRight";
 import { SidebarLeft } from "./components/sidebar-left/SidebarLeft";
@@ -10,25 +13,25 @@ import { SidebarLeft } from "./components/sidebar-left/SidebarLeft";
 import "./index.css";
 
 const App = view(() => {
-	const canvasWrapperRef = useRef<CanvasWrapperElement>(null);
+  const canvasWrapperRef = useRef<CanvasWrapperElement>(null);
 
-	return (
-		<div className="layout">
-			<Header />
-			<div className="content">
-				<div className="sidebar-left">
-					<SidebarLeft />
-				</div>
-				<div className="main-content">
-					<CanvasWrapper ref={canvasWrapperRef} />
-				</div>
-				<div className="sidebar-right">
-					<SidebarRight getCanvas={() => canvasWrapperRef.current.canvas} />
-				</div>
-				<div className="toolbar-top"></div>
-			</div>
-		</div>
-	);
+  return (
+    <div className="layout">
+      <Header />
+      <div className="content">
+        <div className="sidebar-left">
+          <SidebarLeft />
+        </div>
+        <div className="main-content">
+          <CanvasWrapper ref={canvasWrapperRef} />
+        </div>
+        <div className="sidebar-right">
+          <SidebarRight getCanvas={() => canvasWrapperRef.current.canvas} />
+        </div>
+        <div className="toolbar-top"></div>
+      </div>
+    </div>
+  );
 });
 
 ReactDOM.render(<App />, document.getElementById("app"));
--- browser/vite.config.ts
+++ browser/vite.config.ts
@@ -1,10 +1,11 @@
 import { defineConfig } from "vite";
 import preact from "@preact/preset-vite";
 import { ViteEjsPlugin } from "vite-plugin-ejs";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [preact(), ViteEjsPlugin()],
+  plugins: [preact(), ViteEjsPlugin(), tailwindcss()],
   build: {
     rollupOptions: {
       output: {
